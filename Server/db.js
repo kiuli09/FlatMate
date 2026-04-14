2e7b16cd00df4c6c38410a4f683ab2d94a40caef
Judith Ainuu <ainju753@student.otago.ac.nz>
2026-04-14 23:43:39 +1200

    MODIFY: Use env for db credentials

--- Server/db.js
+++ Server/db.js
@@ -1,14 +1,15 @@
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 // Pull DB configuration from environment variables.
 // Create a connection pool using individual local settings.
-  const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'FlatMate',
-    password: 'postgres',
-    port: 5432,
-    options: "-c search_path=public"
-  });
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  options: "-c search_path=public"
+});
 
 module.exports = pool;
