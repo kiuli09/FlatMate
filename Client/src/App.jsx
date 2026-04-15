fe5b141c23f4f8ac6121a934cb7e614be00e7ecb
Judith Ainuu <ainju753@student.otago.ac.nz>
2026-04-15 18:32:19 +1200

    ADD: Add route for shopping list

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import ShoppingList from "./components/shoppinglist";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
@@ -11,6 +12,10 @@ function App() {
             <Route
                 path="/"
                 element={user ? <Dashboard user={user} /> : <Navigate to="/signin" replace />}
+            />
+            <Route
+                path="/shoppinglist"
+                element={user ? <ShoppingList user={user} /> : <Navigate to="/signin" replace />}
             />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
