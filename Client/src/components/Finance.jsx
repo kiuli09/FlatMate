b440f9bacb90376b2338674111d975776ecb8306
Sam <samhostad@hotmail.com>
2026-04-21 16:56:01 +1200

    ADD: Start of FInal Finance Page

--- Client/src/components/Finance.jsx
+++ Client/src/components/Finance.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import "./Finance.css";
 import { NavLink } from "react-router-dom";
 
+
 function Finance({ user }) {
+    
+    const [flatmate,setFlatmates] = useState(["Flatmate1","Flatmate3","Flatmate2","Flatmate4"]);
+    const [owes,setOwes] = useState([0,100,100,0])
+    const [paymentSplit, setPaymentSplit] = useState([25,25,25,25])
+    const [equalSplit, setEqalSplit] = useState(true)
+    const [currentAmount, setCurrentAmount] = useState(0)
+
+    const updateCurrentAmount = (event) => {
+        setCurrentAmount(event.target.value)
+        console.log(currentAmount)
+    }
+
+    const updateEqualSplit = (event) => {
+        setEqalSplit(event.target.checked)
+        console.log(equalSplit)
+    }
+
+    const updatePaymentSplit = (event,index) => {
+        setPaymentSplit([...paymentSplit.slice(0,index),Number(event),...paymentSplit.slice(index+1)])
+    }
+
+    const addTransaction = (event) => {
+        let temp = owes
+        for(let i = 0; i < owes.length; i++){
+            temp+=currentAmount/4
+            }
+        setOwes(temp)
+    }
+// setFlatmates(["Flatmate1","Flatmate3","Flatmate2"]);
     return (
         <div className="dashboard-page">
             <header className="topbar">
@@ -35,16 +66,70 @@ function Finance({ user }) {
                 </aside>
 
                 <main className="main-content">
-                    <div className="welcome-section">
-                        <h2>Finance</h2>
-                        <p>HelloWorld</p>
+                    <h2>Finance</h2>
+                    <div className="finance-content">
+                        <div className="finance-element">
+                            <h2>What your flatmates owe you</h2>
+                            <table >
+                                <tbody>
+                                    {flatmate.map((currentFlatmate,x) => (
+                                        <tr key={x}>
+                                            <td>
+                                                {currentFlatmate} owes you ${owes[x]}
+                                            </td>
+                                            <td>
+                                                {owes[x] === 0 ? "" : "for examplecost"}
+                                            </td>
+                                        </tr>    
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        
+                        <div className="finance-element">
+                            <h2>
+                                Add Transaction
+                            </h2>
+                            <input 
+                                type="number"
+                                placeholder = "Enter amount" 
+                                onChange={setCurrentAmount}
+                            />
+                            <p>Who is paying?</p>
+                            <input 
+                                type="checkbox" 
+                                name="isEqualSplit" 
+                                id="equalSplitCheck" 
+                                checked = {equalSplit}
+                                onChange={updateEqualSplit}
+                            />
+                            <label For="isEqualSplit">Everyone</label>
+                            {equalSplit ? (
+                                <p>temp</p>
+                            ) : (
+                                <div>
+                                    {flatmate.map((currentFlatmate,x) => (
+                                        <div>
+                                            <input 
+                                                type="number"
+                                                id = {"box"+x}
+                                                key = {"box"+x}
+                                                value = {paymentSplit[x]}
+                                                name={x}
+                                                onChange={(e) => updatePaymentSplit(e.target.value,x)}
+                                            />
+                                            <label For={"box"+x}>{currentFlatmate}</label>
+                                        </div>
+                                    ))}
+                                </div> )}
+                            <p>Submit</p>
+                            <input type="button" value="Add Transaction" onClick={addTransaction}/>
+                        </div>
                     </div>
                 </main>
             </div>
         </div>
-
     )
-
 }
 
 export default Finance;
