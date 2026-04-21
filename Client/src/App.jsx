a563eb9ec94528673ad7ad9a742c86bbba958b50
Sam <samhostad@hotmail.com>
2026-04-21 19:14:51 +1200

    FIXED: Merge Error on App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,7 +7,6 @@ import SignUp from "./components/SignUp";
 import Inventory from "./components/Inventory";
 import Finance from "./components/Finance";
 import HomePage from "./components/HomePage";
-import Finance from "./components/Finance";
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -39,29 +38,6 @@ function App() {
             />
 
             <Route
-<<<<<<< HEAD
-                path="/dashboard"
-                element={user ? <Dashboard user={user} /> : <Navigate to="/signin" replace />}
-            />
-
-            <Route
-                path="/shoppinglist"
-                element={user ? <ShoppingList user={user} /> : <Navigate to="/signin" replace />}
-            />
-
-            <Route
-                path="/inventory"
-                element={user ? <Inventory user={user} /> : <Navigate to="/signin" replace />}
-            />
-
-            <Route
-                path="/finance"
-                element={user ? <Finance user={user} /> : <Navigate to="/signin" replace />}
-            />
-
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-=======
                 element={user ? <Navigation user={user} /> : <Navigate to="/signin" replace />}
             >
                 <Route path="/dashboard" element={<Dashboard user={user} />} />
@@ -69,7 +45,6 @@ function App() {
                 <Route path="/shoppinglist" element={<ShoppingList user={user} />} />
                 <Route path="/inventory" element={<Inventory user={user} />} />
             </Route>
->>>>>>> main
         </Routes>
     );
 }
