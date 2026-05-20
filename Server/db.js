6431bb7132521b0d4e571733f6a455070fc00fd0
Judith Ainuu <ainju753@student.otago.ac.nz>
2026-05-21 01:53:27 +1200

    FIX: Db fix for deployment build

--- Server/db.js
+++ Server/db.js
@@ -1,15 +1,11 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// Pull DB configuration from environment variables.
-// Create a connection pool using individual local settings.
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  options: "-c search_path=public"
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
